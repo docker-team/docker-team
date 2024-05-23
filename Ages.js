ee07fa03ed88f6b09a3ed2cfd05cca75720877fb
Yordan Valkov <yordan.valkov@gmail.com>
2024-05-23 19:32:13 +0300

    Edit ages.js

--- Ages.js
+++ Ages.js
@@ -2,9 +2,7 @@ function solve(ages) {
     let result = "";
     if (ages >= 0 && ages <= 2) {
       result = "baby";
-    } else if (ages >= 3 && ages <= 13) {
-      result = "child";
-    } else if (ages >= 14 && ages <= 19) {
+    }  else if (ages >= 14 && ages <= 19) {
       result = "teenager";
     } else if (ages >= 20 && ages <= 65) {
       result = "adult";
